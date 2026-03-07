a2814ffe091fdbf0689f6f72594363dbf0728a2d
KacperZurawik <kaacper181@gmail.com>
2026-03-07 14:49:44 +0100

    add: Book properties and created ctor

--- LibSysConsoleApp/Models/Book.cs
+++ LibSysConsoleApp/Models/Book.cs
@@ -8,5 +8,15 @@ namespace LibSysConsoleApp.Models
 {
     class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(int id, string title, string author)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+        }
     }
 }
