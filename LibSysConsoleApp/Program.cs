93d51ee150129238e1870fc8435fbfcd0cae0e85
KacperZurawik <kaacper181@gmail.com>
2026-03-07 13:56:10 +0100

    Initial commit

---
+++ LibSysConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibSysConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
