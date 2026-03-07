1ca167a3d5e9d4a1e566f25f1749164561343348
KacperZurawik <kaacper181@gmail.com>
2026-03-07 14:28:28 +0100

    add: simple menu

--- LibSysConsoleApp/Program.cs
+++ LibSysConsoleApp/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isAppRunning = true;
+            while (isAppRunning)
+            {
+
+                Console.WriteLine("LibSys - Library Management System");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("");
+
+                Console.WriteLine("1. Add book");
+                Console.WriteLine("2. Exit");
+
+                string menuOption = Console.ReadLine();
+
+            
+                switch (menuOption)
+                {
+                    case "1":
+                        Console.WriteLine("Option 1");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Goodbye!");
+                        isAppRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("There is no such option. Please chose between 1 and 2.");
+                        break;
+                }
+            }
         }
     }
 }
