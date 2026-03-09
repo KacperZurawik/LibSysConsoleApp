01b6d01855a60da20c319f37695652ebb7961568
KacperZurawik <kaacper181@gmail.com>
2026-03-09 22:51:15 +0100

    add: new property for book model

--- LibSysConsoleApp/Models/Book.cs
+++ LibSysConsoleApp/Models/Book.cs
@@ -11,12 +11,14 @@ namespace LibSysConsoleApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public bool IsBorrowed { get; set; }
 
         public Book(int id, string title, string author)
         {
             Id = id;
             Title = title;
             Author = author;
+            IsBorrowed = false;
         }
     }
 }
